7a0e350a9be214b53769fb09e1faa7536cd4f90a
orriannafizz <orriannafizz@gmail.com>
2023-05-08 21:11:48 +0800

    fix api/lifts/type add tables add addfollows

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import Login from "../components/Login";
+import axios from "axios";
+import LiftsData from "../components/LiftsData";
+import AddFollows from "../components/AddFollows";
 const Home = () => {
   const { data: session } = useSession();
 
@@ -9,6 +12,10 @@ const Home = () => {
       <Login />
       <div className="items-center flex flex-col justify-center">
       </div>
+      <LiftsData type="bench" />
+      <LiftsData type="deadlift" />
+      <LiftsData type="squat" />
+      <AddFollows />
     </div>
   );
 };
